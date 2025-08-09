28005ca5850f5e484aa213f67b2fb08223753637
pooyan <thepooyan@gmail.com>
2025-08-09 17:45:30 +0330

    nothing!

--- src/components/icons/Telegram.tsx
+++ src/components/icons/Telegram.tsx
@@ -3,7 +3,7 @@ export default function BsTelegram(props:any) {
     <svg
       {...props}
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 16 16"
       height="1em"

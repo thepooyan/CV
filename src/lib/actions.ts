c42f0fae4565d9e8bb389f9d5f46ec995cbcf9ba
pooyan <thepooyan@gmail.com>
2025-08-29 18:40:23 +0330

    nothing!

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -8,6 +8,7 @@ import { eq, sql } from "drizzle-orm"
 import { cookies } from "next/headers"
 import { cookieNames } from "./constants"
 import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies"
+import { get } from "http"
 
 export async function sendMessage(name: string, email: string, msg: string) {
 
@@ -31,11 +32,10 @@ export async function sendMessage(name: string, email: string, msg: string) {
 
 export const likePostToggle = async (postId: number) => {
   const cookieStore = await cookies()
-  const {add, remove} = handleCookie(cookieStore)
-  const liked = cookieStore.get(cookieNames.likedArticles)
-  const f = liked?.value ? JSON.parse(liked?.value) as string[] : undefined
+  const {add, remove, has} = handleLikedPostsCookie(cookieStore)
+
   try {
-    if (f?.includes(String(postId))) {
+    if (has(String(postId))) {
       await db.update(blogsTable).set({likeCount: sql`${blogsTable.likeCount}-1`}).where(eq(blogsTable.id, postId))
       remove(String(postId))
     } else {
@@ -48,27 +48,30 @@ export const likePostToggle = async (postId: number) => {
   }
 }
 
-const handleCookie = (cookieStore: ReadonlyRequestCookies) => {
-  let c = cookieStore.get(cookieNames.likedArticles)
-  let parsed = c ? JSON.parse(c.value) as string[] : undefined
+const handleLikedPostsCookie = (cookieStore: ReadonlyRequestCookies) => {
+  let targetCookie = cookieStore.get(cookieNames.likedArticles)
+  let value = targetCookie ? JSON.parse(targetCookie.value) as string[] : undefined
 
-  const set = (arr: string[]) => {
+  const setCookie = (arr: string[]) => {
     cookieStore.set(cookieNames.likedArticles, JSON.stringify(arr), {httpOnly: false, secure: false})
   }
   const add = (toAppend: string) => {
     let already = []
-    if (parsed) {
-      already.push(...parsed)
+    if (value) {
+      already.push(...value)
     }
-    set([...already, toAppend])
+    setCookie([...already, toAppend])
   }
   const remove = (toRemove: string) => {
     let toSet:string[] = []
-    if (parsed) {
-      toSet = parsed.filter(p => p !== toRemove)
+    if (value) {
+      toSet = value.filter(p => p !== toRemove)
     }
-    set(toSet)
+    setCookie(toSet)
+  }
+  const has = (str: string) => {
+    return value ? value.includes(str) : false
   }
 
-  return {add,remove}
+  return {add,remove,value,has}
 }

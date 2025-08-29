236b0ad258f3ac9a3ee51db3eee348da9719e35f
pooyan <thepooyan@gmail.com>
2025-08-29 22:06:26 +0330

    nothing!

--- src/app/[lang]/Blog/page.tsx
+++ src/app/[lang]/Blog/page.tsx
@@ -1,12 +1,24 @@
 import BlogFetcher from "@/components/parts/BlogFetcher"
 // import Newsletter from "@/components/parts/Newsletter"
 import Blogs from "@/components/skeleton/Blogs"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
 import { Suspense } from "react"
 
 export default async function BlogPage() {
   return (
     <>
       <div className="container mx-auto px-4 py-8">
+        <Breadcrumb className="mb-5">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/en">Portfolio</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Blog</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
         {/* Hero Section */}
         <div className="text-center mb-12">
           <h1 className="text-4xl md:text-5xl font-bold mb-4 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">

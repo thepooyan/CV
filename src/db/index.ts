4dc8e7ebbe98349c3e44db0e54e315fd47231e49
pooyan <thepooyan@gmail.com>
2025-09-04 12:12:59 +0330

    new db instance on local

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 
+const isProd = process.env.NODE_ENV === 'production'
+
+
 export const db = drizzle({ 
-  connection: { 
+  connection: isProd ? { 
     url: process.env.TURSO_DATABASE_URL!, 
     authToken: process.env.TURSO_AUTH_TOKEN!
-  }
+  } : {
+      url: 'file:./local.sqlite',
+    }
 });
